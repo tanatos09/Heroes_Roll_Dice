ad68bd2298f6d2ed7087efd7b35af6d25c56d592
tanatos09 <tanatos09@gmail.com>
2024-07-20 20:51:50 +0200

    main created - a very rough prototype has been created

--- main.py
+++ main.py
@@ -1,1 +1,21 @@
-#main file
+import tkinter as tk
+from gui import GameGUI
+import characters
+from ui import GameUI
+
+
+def main():
+    player = characters.TestWarrior('Hero', 100, 0, 0, 1, 1, 0)
+    enemy = characters.TestWarrior('Goblin', 100, 0, 0, 1, 1, 0)
+
+    game_ui = GameUI(player, enemy)
+
+    root = tk.Tk()
+
+    game_gui = GameGUI(root, game_ui)
+
+    root.mainloop()
+
+
+if __name__ == "__main__":
+    main()
