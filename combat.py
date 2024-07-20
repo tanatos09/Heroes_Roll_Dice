fbf70927d08d1178c3aea688422572d8bc40c1c8
tanatos09 <tanatos09@gmail.com>
2024-07-20 16:28:02 +0200

    basic combat system

--- combat.py
+++ combat.py
@@ -1,1 +1,28 @@
-#combat file
+import characters
+
+class Combat:
+    def __init__(self, player, enemy):
+        self.player = player
+        self.enemy = enemy
+
+    def battle(self):
+        print(f'The battle between {self.player.name}, and {self.enemy.name}')
+        while self.player.is_alive() and self.enemy.is_alive():
+            #Player's turn
+            self.player.attack(self.enemy)
+            if not self.enemy.is_alive:
+                print(f'{self.enemy.name} is defeated!')
+                break
+
+            self.enemy.attack(self.player)
+            if not self.player.is_alive():
+                print(f'{self.player.name} is defeated!')
+                break
+
+        print('Battle is over')
+
+if __name__ == '__main__':
+    player = characters.TestWarrior('Hero', 100, 0, 0,1, 1,0)
+    enemy = characters.TestWarrior('Goblin', 100, 0, 0,1, 1,0)
+    combat = Combat(player, enemy)
+    combat.battle()
