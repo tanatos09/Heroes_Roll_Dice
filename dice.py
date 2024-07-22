342eb4deaf373e65f332bf90ceee38afb109100c
tanatos09 <tanatos09@gmail.com>
2024-07-22 15:33:24 +0200

    added D12, D20, D100 and magic dice

--- dice.py
+++ dice.py
@@ -14,6 +14,22 @@ class Dice:
         '''
         return random.randint(1, self.sides)
 
+    def multi_roll(self, times):
+        '''
+
+        :param times: number of dice rolls
+        :return: a list of dice roll values
+        '''
+        return [self.roll() for _ in range(times)]
+
+    def result_text(self, result):
+        '''
+
+        :param result: result of the dice roll
+        :return: text of the result
+        '''
+        return f'You rolled the dice for: {result}'
+
 #define 6-sided dice
 class D6(Dice):
     def __init__(self):
@@ -23,9 +39,57 @@ class D10(Dice):
     def __init__(self):
         super().__init__(sides=10) #10 sides
 
+class D12(Dice):
+    def __init__(self):
+        super().__init__(sides=12) #12 sides
+
+class D20(Dice):
+    def __init__(self):
+        super().__init__(sides=20) #20 sides
+
+class D100(Dice):
+    def __init__(self):
+        super().__init__(sides=100) #100 sides
+
+class MagicDice(Dice):
+    def __init__(self, attacks):
+        '''
+
+        :param attacks: list of magics
+        '''
+        self.attacks = attacks
+
+    def roll(self):
+        '''
+        select magic
+        :return: a random magic
+        '''
+        return random.choice(self.attacks)
+
+    def result_text(self, result):
+        '''
+
+        :param result: selected magic
+        :return: text of the selected special attack
+        '''
+        return f'Your magic dice roll: {result}'
+
+
 if __name__ == '__main__':
     d6 = D6()
     d10 = D10()
+    d12 = D12()
+    d20 = D20()
+    d100 = D100()
+    magic = ['Shield', 'Heal', 'Instant Kill', 'None']
+    magic_dice = MagicDice(magic)
 
-    print(f'Rolling a D6: {d6.roll()}')
-    print(f'Rolling a D10: {d10.roll()}')
+    print(d6.result_text(d6.roll()))
+    print(d10.result_text(d10.roll()))
+    print(d12.result_text(d12.roll()))
+    print(d20.result_text(d20.roll()))
+    print(d100.result_text(d100.roll()))
+    print(f'Your roll of three D10 dice: {d10.multi_roll(3)}')
+    use_magic_result = magic_dice.roll()
+    print(magic_dice.result_text(use_magic_result))
+
