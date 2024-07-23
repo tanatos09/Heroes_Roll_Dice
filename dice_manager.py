f9a8f1fac4128576b84a2a4d5c6e35aa718da54a
tanatos09 <tanatos09@gmail.com>
2024-07-23 18:34:03 +0200

    created comments

--- dice_manager.py
+++ dice_manager.py
@@ -2,7 +2,13 @@ import random
 from dice import D6, D10, D12, D20, D100, MagicDice
 
 class DiceManager:
+    '''
+    manages diffrent types of dice rolls
+    '''
     def __init__(self):
+        '''
+        dice types
+        '''
         self.dice_types = {
             'D6': D6(),
             'D10': D10(),
@@ -12,12 +18,29 @@ class DiceManager:
         }
 
     def clasic_roll(self,dice):
+        '''
+        clasic roll with given dice
+        :param dice: dice to roll
+        :return: random int between 1 and the number of sides
+        '''
         return random.randint(1, dice.sides)
 
     def magic_roll (self, magic_dice):
+        '''
+        magic roll with given magic dice
+        :param magic_dice: magic dice object to roll
+        :return: random spell from magic dice
+        '''
         return random.choice(magic_dice.spells)
 
     def multi_roll (self, dice_type, times):
+        '''
+
+        perform multiple rolls of the specified dice type
+        :param dice_type: select dice (str)
+        :param times: how many to roll
+        :return: list of dice rolls
+        '''
         if dice_type in self.dice_types:
             return [self.clasic_roll(self.dice_types[dice_type]) for _ in range(times)]
         else:
